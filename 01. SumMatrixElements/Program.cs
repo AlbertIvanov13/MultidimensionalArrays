40613458932c1ab43ea2c216b6b33f3df21725cc
Albert Ivanov <albertivanov187@gmail.com>
2025-02-06 22:52:34 +0200

    Add project files.

---
+++ 01. SumMatrixElements/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+int rows = input[0];
+int cols = input[1];
+
+int[,] matrix = new int[rows, cols];
+
+int sum = 0;
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+	int[] elements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+	for (int col = 0; col < matrix.GetLength(1); col++)
+	{
+		matrix[row, col] = elements[col];
+		sum += elements[col];
+	}
+}
+
+Console.WriteLine(rows);
+Console.WriteLine(cols);
+Console.WriteLine(sum);
