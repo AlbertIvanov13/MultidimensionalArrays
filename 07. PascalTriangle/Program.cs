2de2a1985b1de4ad2338aff0d2dfefe39f3b7c9a
Albert Ivanov <albertivanov187@gmail.com>
2025-02-09 17:25:35 +0200

    100/100

---
+++ 07. PascalTriangle/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Data;
+using System.Runtime.InteropServices;
+
+int triangleSize = int.Parse(Console.ReadLine());
+
+long[][] jaggedArray = new long[triangleSize][];
+
+int sum = 0;
+for (int row = 0; row < jaggedArray.Length; row++)
+{
+	if (row == 0)
+	{
+		jaggedArray[row] = new long[1];
+		jaggedArray[row][0] = 1;
+	}
+}
+
+bool isSet = false;
+for (int row = 1; row <= jaggedArray.Length - 1; row++)
+{
+	isSet = false;
+	jaggedArray[row] = new long[jaggedArray[row - 1].Length + 1];
+	for (int col = 0; col < jaggedArray[row - 1].Length; col++)
+	{
+        if (!isSet)
+        {
+            jaggedArray[row][0] = 1;
+            jaggedArray[row][jaggedArray[row].Length - 1] = 1;
+            isSet = true;
+        }
+    }
+}
+
+for (int row = 2; row <= jaggedArray.Length - 1; row++)
+{
+	for (int col = 1; col <= jaggedArray[row].Length - 1; col++)
+	{
+		if (jaggedArray[row][col] == 0)
+		{
+			jaggedArray[row][col] = jaggedArray[row - 1][col] + jaggedArray[row - 1][col - 1];
+		}
+	}
+}
+
+for (int row = 0; row < jaggedArray.Length; row++)
+{
+	for (int col = 0; col < jaggedArray[row].Length; col++)
+	{
+		Console.WriteLine(string.Join(" ", jaggedArray[row]));
+		break;
+	}
+}
